90a0a1309a6a9c990db7fbbe91baee508180f537
Snowman1024 <guoqingfu1987@163.com>
2020-10-22 09:34:55 +0800

    'snowflake算法，生成分布式id'

---
+++ src/main/java/com/snowman/snowflake/SnowflakeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.snowman.snowflake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowflakeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowflakeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snowman/snowflake/SnowflakeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowman.snowflake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowflakeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
